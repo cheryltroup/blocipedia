7ca32c0d7889d12a774c0ed3a8cda4a3d3fbd6a2
cheyltroup <cheryltroup@gmail.com>
2015-07-28 17:52:39 -0700

    Wiki_CRUD_Part2 Completed"

--- app/controllers/wikis_controller.rb
+++ app/controllers/wikis_controller.rb
@@ -8,11 +8,45 @@ class WikisController < ApplicationController
   end
 
   def new
+    @wiki = Wiki.new
   end
+
+  def create
+     @wiki = Wiki.new(params.require(:wiki).permit(:title, :body))
+     #raise # this will short-circuit the method
+     if @wiki.save
+       flash[:notice] = "Wiki was saved."
+       redirect_to @wiki
+     else
+       flash[:error] = "There was an error saving the wiki. Please try again."
+       render :new
+     end
+   end
 
   def edit
+    @wiki = Wiki.find(params[:id])
   end
 
+  def update
+    @wiki = Wiki.find(params[:id])
+    if @wiki.update_attributes(params.require(:wiki).permit(:title, :body))
+      flash[:notice] = "Wiki was updated."
+      redirect_to @wiki
+    else
+      flash[:error] = "There was an error saving the wiki. Please try again."
+      render :edit
+     end
+   end
+
   def destroy
+    @wiki = Wiki.find(params[:id])
+    if @wiki.destroy
+      flash[:notice] = "Wiki was sucessfully deleted."
+      redirect_to @wiki
+    else
+      flash[:error] = "There was an error deleting the wiki."
+      render :show
+    end
   end
+
 end

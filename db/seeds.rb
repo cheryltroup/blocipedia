d10fd51d20fe44f0c97c30644c7112c38c697946
cheyltroup <cheryltroup@gmail.com>
2015-07-30 12:23:29 -0700

    Seeding_Data Completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,7 +9,7 @@
 require 'faker'
  
  # Create Wikis
- 50.times do
+ 55.times do
    Wiki.create!(
      title:  Faker::Lorem.sentence,
      body:   Faker::Lorem.paragraph
@@ -17,29 +17,46 @@ require 'faker'
  end
  wikis = Wiki.all
 
- # Create an admin user
- admin = User.new(
-   name:     'Admin User',
-   email:    'admin@example.com',
-   password: 'helloworld',
-   role:     'admin'
+  #Create an admin user
+  admin = User.new(
+  name:     'Admin User',
+  email:    'admin@example.com',
+  password: 'helloworld',
+  role:     'admin'
  )
  admin.skip_confirmation!
  admin.save!
-
  
- # Create a member
+ # Create a member 1
  member = User.new(
-   name:     'Member User',
-   email:    'member@example.com',
+   name:     'Member User1',
+   email:    'member1@example.com',
    password: 'helloworld',
    role:     'standard'
  )
  member.skip_confirmation!
  member.save!
 
+ # Create a member 2
+ member = User.new(
+   name:     'Member User2',
+   email:    'member2@example.com',
+   password: 'helloworld',
+   role:     'standard'
+ )
+ member.skip_confirmation!
+ member.save!
  
- 
+  # Create a member 3
+ member = User.new(
+   name:     'Member User3',
+   email:    'member3@example.com',
+   password: 'helloworld',
+   role:     'standard'
+ )
+ member.skip_confirmation!
+ member.save!
+
  puts "Seed finished"
  puts "#{Wiki.count} wikis created"
  puts "#{User.count} user created"
